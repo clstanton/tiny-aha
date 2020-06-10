18e20d4d18173e0d164e76c46443e13796420de3
clstanton <cstanton77@gmail.com>
2020-06-10 08:11:07 -0500

    heroku created

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+// route client will have to fetch from //
+const {notes} = require('./data/db');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// callback function that will execute every time route is accessed with a get request //
+app.get('/api/db', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-  });
+});
