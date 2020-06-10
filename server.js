dfb798070969237a689e4ad93aaa1ce4f2920d55
clstanton <cstanton77@gmail.com>
2020-06-10 15:28:58 -0500

    middleware added

--- server.js
+++ server.js
@@ -5,10 +5,32 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data //
+app.use(express.urlencoded({extended: true}));
+
+// parse incoming JSON data //
+app.use(express.json());
+
 // callback function that will execute every time route is accessed with a get request //
 app.get('/api/db', (req, res) => {
     let results = notes;
     res.json(results);
+});
+
+app.get('/api/db/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+// endpoint that accepts data from the client to be stored on the server //
+app.post('/api/db', (req, res) => {
+    // req.body is where the incoming content will be //
+    console.log(req.body);
+    res.json()
 });
 
 app.listen(PORT, () => {
