da07c97a71e7c4ef1ecea7c593b68bf080c69d89
clstanton <cstanton77@gmail.com>
2020-06-10 18:50:06 -0500

    write to json file

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// write data to db.json
+const fs = require('fs');
+const path = require('path');
+
 // route client will have to fetch from //
 const {notes} = require('./data/db');
 
@@ -5,20 +9,52 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data //
-app.use(express.urlencoded({extended: true}));
+/// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
 
-// parse incoming JSON data //
+// parse incoming JSON data
 app.use(express.json());
 
-// callback function that will execute every time route is accessed with a get request //
+// takes id & array of notes and returns a single note object //
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+}
+
+// Function to handle note creation //
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/db.json'),
+        JSON.stringify({notes: notesArray}, null, 2)
+    );
+
+    // Return finished code to post route for response //
+    return note;
+}
+
+// filter function //
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if (query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title);
+      }
+      return filteredResults;
+}
+
+// argument 1. string that describes the route the client will have to fetch from //
+// argument 2. callback function that will execute every time route is accessed with a get request //
 app.get('/api/db', (req, res) => {
     let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query,results);
+    }
     res.json(results);
 });
 
 app.get('/api/db/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
+    const result = findById(req.params.id, animals);
     if (result) {
         res.json(result);
     } else {
@@ -29,8 +65,13 @@ app.get('/api/db/:id', (req, res) => {
 // endpoint that accepts data from the client to be stored on the server //
 app.post('/api/db', (req, res) => {
     // req.body is where the incoming content will be //
-    console.log(req.body);
-    res.json()
+    // set id based on what the next index of the array will be //
+    req.body.id = notes.length.toString();
+
+    //add note to JSON file and notes array in this function //
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
 });
 
 app.listen(PORT, () => {
